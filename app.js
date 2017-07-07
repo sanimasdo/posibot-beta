69e456828da8e89dd58dbb4e98dcd4a4dfd95a03
NoahCSstudent <noah.cohen.matlofsky@mail.utoronto.ca>
2017-07-07 00:27:33 -0400

    8ball support

--- app.js
+++ app.js
@@ -162,7 +162,7 @@ client.on('message', message => {
         message.channel.send(randomRape[pick]);
     } else
 
-=   if (message.content.toLowerCase().startsWith(prefix + '8ball')) {
+    if (message.content.toLowerCase().startsWith(prefix + '8ball')) {
         var random8ball = [`It is certain.`, `It is decidedly so.`, `Without a doubt.`, `Yes definitely.`, `You may rely on it.`,
 			`As I see it, yes.`, `Most likely.`, `Outlook good.`, `Yes.`, `Signs point to yes.`, `Reply hazy try again.`,
 			`Better not tell you now.`, `Cannot predict now.`, `Concentrate and ask again.`, `Don't count on it.`, `My reply is no.`,
