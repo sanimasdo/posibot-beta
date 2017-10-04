a3b9dfbdb4002af58c8ecf4e48aedf0a494607d8
RBmemeking <raisin.bran1995@gmail.com>
2017-10-04 09:48:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"); //
 const client = new Discord.Client();
 const randomInt = require('random-int');
 const yt = require('ytdl-core');
